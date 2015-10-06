3a1121edb2392cfda601d2b8cff18c3176710c8a
GrupoCinco <brian0592@gmail.com>
2015-10-05 22:43:00 -0300

    Update Cls_Project_Cesar.java

--- Proyecto_Cifrado_Cesar/src_cifrado_cesar/Package_Cesar/Cls_Project_Cesar.java
+++ Proyecto_Cifrado_Cesar/src_cifrado_cesar/Package_Cesar/Cls_Project_Cesar.java
@@ -12,61 +12,61 @@ import java.io.IOException;
 
 public class Cls_Project_Cesar implements Interface_Cifrado_Cesar{
 		public char array[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M'
-							  ,'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+				      ,'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 		Cls_Project_Cesar()
 		{
 		}
 		
 		private char getTextoCifrado(char parTextoClaro,int dezpl)
 		{
-        int indiceX = 0;
-        for (int i = 0; i < array.length; i++)
-        {
-            if (parTextoClaro ==array[i]) {
-                indiceX = i;
-                break;
-            }
-        }
-        return array[(indiceX + dezpl) % 27];
+        		int indiceX = 0;
+        		for (int i = 0; i < array.length; i++)
+			{
+        			if (parTextoClaro ==array[i]) {
+        			indiceX = i;
+        			break;
+            			}
+        		}
+		 return array[(indiceX + dezpl) % 27];
 		}
 		
 		private char getTextoClaro(char parTextoCifrado,int dezpl)
 		{
-        int indiceX = 0;
-        for (int i = 0; i < array.length; i++)
-        {
-            if (parTextoCifrado == array[i]){
-                indiceX = i;
-                break;
-            }
-        }
-        return array[(indiceX +(27-dezpl)) % 27];
+        		int indiceX = 0;
+        		for (int i = 0; i < array.length; i++)
+			{
+            			if (parTextoCifrado == array[i]){
+                			indiceX = i;
+                			break;
+            			}
+        		}
+        		return array[(indiceX +(27-dezpl)) % 27];
 		}
 		
 		public String encriptarTextoClaro(String textoClaro,int dezpl)
 		{
-	        String textoCifrado = "";
-	        for(int i=0; i<textoClaro.length(); i++)
-	        {
-	            if(textoClaro.charAt(i) != ' ')
-	                textoCifrado += this.getTextoCifrado(textoClaro.charAt(i),dezpl)+"";
-	            else
-	                textoCifrado += " ";
-	        }
-	        return textoCifrado;
+	        	String textoCifrado = "";
+	        	for(int i=0; i<textoClaro.length(); i++)
+	        	{
+	            		if(textoClaro.charAt(i) != ' ')
+	                	textoCifrado += this.getTextoCifrado(textoClaro.charAt(i),dezpl)+"";
+	        	else
+	                	textoCifrado += " ";
+	        	}
+	        	return textoCifrado;
 		} 
 		
 		public String desencriptarTextoCifrado(String textoCifrado,int dezpl)
 		{
-	        String textoClaro = "";
-	        for(int i=0; i<textoCifrado.length(); i++){
-	            if(textoCifrado.charAt(i) != ' ')
-	                textoClaro += this.getTextoClaro(textoCifrado.charAt(i),dezpl)+"";
-	            else
-	                textoClaro += " ";
-	        }
-	        return textoClaro;
-	    }    
+			 String textoClaro = "";
+	        	 for(int i=0; i<textoCifrado.length(); i++){
+	            		if(textoCifrado.charAt(i) != ' ')
+	                	textoClaro += this.getTextoClaro(textoCifrado.charAt(i),dezpl)+"";
+	            	 else
+	                	textoClaro += " ";
+	        	 }
+	        	return textoClaro;
+	    	}    
 		
 		public void muestraFile(String archivo) throws FileNotFoundException, IOException
 		{
@@ -75,7 +75,7 @@ public class Cls_Project_Cesar implements Interface_Cifrado_Cesar{
 			BufferedReader b = new BufferedReader(f);
 			while((cadena = b.readLine())!=null)
 			{
-            System.out.println(cadena);
+        		 System.out.println(cadena);
 			}
 			b.close();
 		}
@@ -84,9 +84,9 @@ public class Cls_Project_Cesar implements Interface_Cifrado_Cesar{
 		{
 			/** 
 		  	Determina si una palabra es valida
-		  	archivo: archivo file que contiene lista de las palabras en el diccionario(Español).
+		  	archivo: archivo file que contiene lista de las palabras en el diccionario(EspaÃ±ol).
   		  	palabras: una posible palabra.
-          	retorna Verdadero si la palabra está en la lista.
+          	retorna Verdadero si la palabra estÃ¡ en la lista.
 			 **/
 			String cadena;
 			palabra=palabra.toLowerCase();
